169784af323b1448af03a588443ec300d54aa70d
Cezary Nowak <cezary.nowak@gmail.com>
2024-10-14 15:39:22 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# sanitize-html
-Simple helper for HTML sanitization. Browser only as for now.
+# simple-sanitize-html
+Simple helper for HTML sanitization.
+Browser only as for now.
+
+## Features
+- cleaning up HTML code. Unclosed tags are closed. Invalid markup is fixed.
+- Library is small. No twisted regexps.
+- It's secure. We are using browser to do all the magic for us.
+
+# API
+```
+sanitizeHTMLString(htmlString, disallowedTags);
+```
+
+For invalid `htmlString` an error is thrown: 'Invalid template string value'
+
+Default `disallowedTags` are `['SCRIPT', 'STYLE']``
+
+
+## Usage
+```
+import satitizeHTML from 'simple-satnitize-html';
+
+satitizeHTML('<test><script>alert("nope")</script></test>');
+// '<test></test>',
+
+satitizeHTML('<test onclick="() => {}"></test>');
+// '<test></test>',
+
+satitizeHTML('a<style>body { rotate3d(0, -14, 11, 53deg) }</style>b');
+// 'ab',
+
+satitizeHTML('<!--');
+// '<!---->',
+
+satitizeHTML('<test ONMOUSEOVER="() => {}"></test>');
+// '<test></test>',
+
+satitizeHTML('abc <strong onclick="alert(\'\')" onmouseover="alert(\'\')">strong</strong> xyz');
+// 'abc <strong>strong</strong> xyz',
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * Sanitize HTML elements in the template string
+ * Removes <script>, <style> and html elements with event handler - OL-2342
+ * Clears tags that are not properly closed
+ *
+ * @param   {string}  html  Text that contains html elements
+ * @param   {Array<uppercase strings>}  disallowed tags. Default ['SCRIPT', 'STYLE']
+ *
+ * @return  {string}        Sanitized text with html elements
+ */
+module.exports = sanitizeHTMLString(html, disallowedTags = ['SCRIPT', 'STYLE']) {
+  if (!html) return '';
+  if (typeof html !== 'string') throw Error('Invalid template string value');
+  let result = html;
+
+  if (html.includes('<')) {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+
+    [...div.querySelectorAll('*')].forEach((children) => {
+      if (disallowedTags.includes(children.tagName)) {
+        return children.remove();
+      }
+
+      const attributes = children.getAttributeNames();
+      attributes.forEach((attr) => {
+        if (attr.startsWith('on')) {
+          children.removeAttribute(attr);
+        }
+      })
+    });
+
+    result = div.innerHTML;
+    div.remove();
+  }
+
+  return result;
+};
